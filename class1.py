69ac143398d1f15a93fd2d79f137be562ccbfa52
Tolon0 <tolon0005@gmail.com>
2023-04-03 01:43:30 +0600

    push

--- class1.py
+++ class1.py
@@ -1,3 +1,11 @@
 class Hello:
     def __init__(self, str):
         self.str = str
+
+class New(Hello):...
+a = New('kskfk')
+
+
+
+
+
--- task.py
+++ task.py
@@ -1,2 +1,5 @@
 from art import tprint
 tprint("PYTHON")
+import class1
+
+tprint(a)
